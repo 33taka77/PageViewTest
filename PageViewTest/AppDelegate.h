71cf91fa7a1fc4edeb59a6ce7c19984331c9e028
Aizawa Takashi <aizawatakashi@Aizawa-no-MacBook-Pro.local>
2014-04-02 08:54:41 +0900

    Initial Commit

---
+++ PageViewTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PageViewTest
+//
+//  Created by Aizawa Takashi on 2014/04/02.
+//  Copyright (c) 2014年 Aizawa Takashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
